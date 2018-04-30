ea6aca8879ba973a1f850a113bea159a7e6003eb
po-ppy <horpoppy@gmail.com>
2018-04-30 14:51:36 +0800

    个人邮箱配置文件

--- config/mail_config_template.rb
+++ config/mail_config_template.rb
@@ -1,5 +1,5 @@
 #_*_ coding:utf-8 _*_
 Mail.defaults do
-  delivery_method :smtp, address: 'smtp.163.com', port: 25, user_name: 'xxxxxxxx@163.com', password: 'password', enable_ssl: false
-  retriever_method :pop3, address: 'pop3.163.com',port: 995, user_name: 'xxxxxxxx@163.com', password: 'password', enable_ssl: true
+  delivery_method :smtp, address: 'smtp.163.com', port: 25, user_name: 'xxxxxxx@163.com', password: 'password', enable_ssl: false
+  retriever_method :pop3, address: 'pop3.163.com',port: 995, user_name: 'xxxxxxx@163.com', password: 'password', enable_ssl: true
 end
