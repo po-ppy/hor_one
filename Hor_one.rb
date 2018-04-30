7f5247dc6bc5457015a3c7f79259a259d0b98413
po-ppy <horpoppy@gmail.com>
2018-04-30 14:42:33 +0800

    解决判断最后一个的问题“

--- Hor_one.rb
+++ Hor_one.rb
@@ -4,14 +4,14 @@ require './config/mail_config.rb'
 
 module Hor_one
   @last_one = nil  
+  @pre_one = nil
 
   def get_one
     emails = Mail.all
     if emails.length < 0
       return 
     end
-    emails = Mail.find(what: :last, count: 15, order: :dec)
-    puts emails.length
+    #emails = Mail.find(what: :last, count: 15, order: :dec)
     emails.each do |email|
       #if @last_one == email || email.subject != "command" || email.subject != "common"
       #  puts email.subject
@@ -22,14 +22,13 @@ module Hor_one
       #  puts @last_one.subject
       #  break
       #end
-      if email.subject == 'common' || email.subject == 'command'
+      if email.subject.to_s.chomp == 'common' || email.subject.to_s.chomp == 'command'
         if @last_one == nil
-          puts "last_one is nil"
           @last_one = email
-          puts @last_one.subject
           break
-        elsif @last_one == email
-          next
+        elsif @last_one.date < email.date
+          @last_one = email
+          break
         end
       else
         next
@@ -41,10 +40,23 @@ module Hor_one
     return @last_one == nil
   end
 
+  def is_new?
+    if @last_one == nil
+      false
+    elsif @pre_one == nil
+      true
+    elsif @pre_one.date < @last_one.date
+      true
+    else
+      false
+    end
+  end
+
   def execute_one
+    @pre_one = @last_one
     if @last_one.subject == 'common'
       #do some common job
-      result = do_common_job @last_one.body
+      result = do_common_job @last_one.body.to_s.chomp
     else
       #do some commmand job
     end
@@ -57,12 +69,13 @@ module Hor_one
   
   def do_common_job(common)
     case common
-    when '签到'
+    when "sign"
       #do yiban sign
       "应该是签到的"
 
-    when '充值'
+    when "water"
       #do nfc copy water_card
+      "充值相关"
     else
       "不支持的命令"
     end
@@ -75,11 +88,12 @@ module Hor_one
     if one_is_null? 
       puts "in feedback last_one is nil"
     end
-    temp = @last_one.from
-    puts temp
+    tempFrom = @last_one.from
+    tempTo = @last_one.to
+    puts tempFrom
     mail = Mail.new do
-      from "18821701674@163.com"
-      to temp
+      from tempTo
+      to tempFrom
       subject "结果反馈"
       body info
     end
--- test.rb
+++ test.rb
@@ -1,5 +1,23 @@
 #_*_ coding:utf-8 _*_
 require './Hor_one'
 include Hor_one
-get_one
-execute_one
+while true do
+  sleep(2)
+  get_one
+  if is_new?
+    execute_one
+  end
+end
+#emails = Mail.all
+#emails = Mail.find(what: :last, count: 15, order: :dec)
+#emails.sort! do |a,b|
+#  b.date <=> a.date
+#end
+#emails.each do |email|
+#  puts "#{email.date.to_s}-->#{email.subject}"
+#end
+
+#get_one
+#execute_one
+#puts "%%%#{@last_one.body.to_s.chomp}%%%"
+#
