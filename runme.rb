e4cdd90e62f929cc2d115535ca8b1e0157cf0202
po-ppy <horpoppy@gmail.com>
2018-05-02 21:38:42 +0800

    防止网络原因而导致服务挂掉

--- runme.rb
+++ runme.rb
@@ -5,16 +5,23 @@ include Hor_one
 runme = Thread.new do
 
   while true do
-    flash_emails
-    get_one
-    #if is_new?
-     # execute_one
-    #end
-    while is_new?
-      execute_one
+    begin
+      flash_emails
       get_one
-    end 
-    sleep 120
+      #if is_new?
+       # execute_one
+      #end
+      while is_new?
+        execute_one
+        get_one
+      end 
+      sleep 120
+
+    rescue Exception => e
+      puts e.message
+      sleep 10
+      retry
+    end
   end
 
 end
